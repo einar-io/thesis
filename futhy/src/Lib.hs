ecdda83e3c41f41ac50e299bc9047e74b07c98a5
ulrik elmelund <ulrikelmelund@gmail.com>
2021-08-26 09:38:38 +0200

    clean and add pair stub

--- futhy/src/Lib.hs
+++ futhy/src/Lib.hs
@@ -1,12 +1,11 @@
 module Lib where
 
-import Prelude
-
 type RealNumb = Float
 
 data Var
   = Scalar RealNumb
   | Tensor [Var]
+  | Pair Var Var
   deriving (Show, Eq)
 
 data LFun
@@ -22,42 +21,28 @@ data BiOp
   | Outer
   deriving (Show, Eq)
 
-outer :: Var -> Var -> Var
-outer (Scalar x) (Scalar y) = Scalar $ x * y
-outer x@(Scalar _) (Tensor ys) = Tensor $ map (outer x) ys
-outer (Tensor xs) v = Tensor $ map (outer v) xs
-
--- dotproduct :: Var -> Var -> Var
--- dotproduct (Tensor a) (Tensor b) = Scalar $ sum $ a * b
-
--- works for dot product, matrix multiplication, 'binary tensor contraction' at https://www.tensors.net/p-tutorial-1
--- pairs last dimension of first variable with first dimension of second tensor
--- please dont use it on scalars...
-generalContraction :: Var -> Var -> Var
-generalContraction a b = case (a,b) of
-  (Tensor x, Tensor y) -> undefined
-  _ -> undefined -- error
+-- general implementation - outer product, no contraction
+mult :: Var -> Var -> Var
+mult (Scalar x) (Scalar y) = Scalar $ x * y
+mult x@(Scalar _) (Tensor ys) = Tensor $ map (mult x) ys
+mult (Tensor xs) y = Tensor $ map (mult y) xs
+mult _ _ = undefined
 
 applyOp :: BiOp -> Var -> Var -> Var
 applyOp op a b = case op of
   Mult -> undefined
-  Outer -> outer a b
+  Outer -> mult a b
 
 interpret :: LFun -> Var -> Var
 interpret f v = case f of
   Id -> v
   Comp f1 f2 -> interpret f2 (interpret f1 v)
-  Scale r -> outer (Scalar r) v
+  Scale r -> mult (Scalar r) v
   LSec v0 op -> applyOp op v0 v
   RSec op v0 -> applyOp op v v0
 
 eval :: LFun -> Var -> String
 eval f v = show (interpret f v)
-
-
-
-
-
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
