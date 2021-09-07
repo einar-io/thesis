1f7ced09d27a5c08021b55f583b655302f20d33f
ulrik elmelund <ulrikelmelund@gmail.com>
2021-09-07 07:31:43 +0200

    fix the tests. compiling a program requires an LFun and a Val to apply it to, compiling a 'bare' LFun is not meaningful anymore, due to the 'arity' of the input determining the semantics of the LFun

--- futhy/src/Compiler.hs
+++ futhy/src/Compiler.hs
@@ -39,45 +39,13 @@ biop b a1 a2 =  let base = case b of
 arityext :: Arity -> Arity -> String
 arityext a1 a2 = "_" <> show(ua a1) <> "_" <> show(ua a2)
 
-lfun :: LFun -> Int -> Arity -> (Int, String, Arity)
-lfun linfun c1 a1 = case linfun of
-  Id -> gen_sloc c1 "id" a1
-  Dup -> gen_sloc c1 "dupe" (P a1 a1)
-  Comp lf2 lf1 -> let (c2, subprog1, a2) = lfun lf1 c1 a1 in
-                  let (c3, subprog2, a3) = lfun lf2 c2 a2 in
-                  let (c4, nloc, a4) = gen_sloc c3 ("comp" <> spacefun <> show(c3-1) <> spacefun <> show(c2-1)) a3
-                  in (c4, subprog2 <> subprog1 <> nloc, a4)
-  Para lf2 lf1 ->
-    case a1 of
-      P a3 a2 ->
-        let (c2, subprog1, a4) = lfun lf1 c1 a2 in
-        let (c3, subprog2, a5) = lfun lf2 c2 a3 in
-        let (c4, nloc, a6) = gen_sloc c3 ("para" <> spacefun <> show(c3-1) <> spacefun <> show(c2-1)) (P a5 a4)
-        in (c4, subprog2 <> subprog1 <> nloc, a6)
-      _ -> undefined --ERROR, argument to para must be a Pair of Vals
-  LSec v b -> gen_sloc c1 (biop b (val_arity v) a1 <> " " <> val v) a1
-  RSec b v -> gen_sloc c1 (val v <> " " <> biop b (val_arity v) a1) a1
-  Scale rn -> lfun (LSec (Scalar rn) Outer) c1 a1
-  Zero -> lfun (Scale 0) c1 a1 --redirection
-  Prj i j -> case (i,j,a1) of
-                (2, 1, P a2 _) -> gen_sloc c1 "fst" a2
-                (2, 2, P _ a2) -> gen_sloc c1 "snd" a2
-                _ -> undefined
-  Lplus lf2 lf1 -> let (c2, subprog, a4@(P a3 a2)) = lfun (Para lf2 lf1) c1 a1 in
-                   let (c3, nloc, a5) = gen_sloc c2 ("plus" <> arityext a3 a2 <> " " <> "fun" <> show(c2-1)) a4
-                   in (c3, subprog <> nloc, a5)
-  Red _ -> undefined
-  Add _ -> undefined
-  LMap _ -> undefined
-  Zip _ -> undefined
-
 
 type Program = String
 type Count = Int
 
 type CState = (Program, Arity, Count)
 newtype Compiler a = Co {runCo :: CState -> (a, CState)}
-instance Functor Compiler
+instance Functor Compiler where
 instance Applicative Compiler
 instance Monad Compiler where
   return a = Co (\cs -> (a, cs))
@@ -90,14 +58,8 @@ initComp cs = Co (\_ -> ((), cs))
 getProgr :: Compiler Program
 getProgr = Co (\cs@(prog, _, _) -> (prog, cs))
 
-getArity :: Compiler Arity
-getArity = Co (\cs@(_, arit, _) -> (arit, cs))
-
 getCountArity :: Compiler (Count, Arity)
 getCountArity = Co (\cs@(_, arit, cnt) -> ((cnt, arit), cs))
-
-getCount :: Compiler Count
-getCount = Co (\cs@(_, _, cnt) -> (cnt, cs))
 
 finishProg :: Compiler ()
 finishProg =
@@ -105,28 +67,24 @@ finishProg =
     let new_loc = "entry main =" <> spacefun <> show(c-1) <> " arg"
     in ((), (p <> new_loc, r, c)))
 
-locMArit :: Arity -> Program -> Compiler ()
-locMArit r fun =
+locM :: Arity -> Program -> Compiler ()
+locM r fun =
   Co (\(p0, _, c0) ->
       let new_loc = "let" <> spacefun <> show(c0) <> " = (" <> fun <> ")" <> nl
       in ((), (p0 <> new_loc, r, c0+1)))
-
-locM :: Program -> Compiler ()
-locM fun = do r <- getArity
-              locMArit r fun
 
 -- to monadically compile a function, give it the function and the arity (type) of the input to that function
 -- the arity of the output of the function then depends on the input and the function itself
 lfunM :: LFun -> Arity -> Compiler ()
 lfunM linfun a1 = case linfun of
-  Id -> locM "id"
-  Dup -> locMArit (P a1 a1) "dupe"
+  Id -> locM a1 "id"
+  Dup -> locM (P a1 a1) "dupe"
   Comp lf2 lf1 -> do
                     lfunM lf1 a1
                     (c2, a2) <- getCountArity
                     lfunM lf2 a2
                     (c3, a3) <- getCountArity
-                    locMArit a3 ("comp" <> spacefun <> show(c3-1) <> spacefun <> show(c2-1))
+                    locM a3 ("comp" <> spacefun <> show(c3-1) <> spacefun <> show(c2-1))
   Para lf2 lf1 ->
     case a1 of
       P a3 a2 -> do
@@ -134,29 +92,25 @@ lfunM linfun a1 = case linfun of
                    (c2, a4) <- getCountArity
                    lfunM lf2 a3
                    (c3, a5) <- getCountArity
-                   locMArit (P a5 a4) ("para" <> spacefun <> show(c3-1) <> spacefun <> show(c2-1))
+                   locM (P a5 a4) ("para" <> spacefun <> show(c3-1) <> spacefun <> show(c2-1))
       _ -> undefined --ERROR, argument to para must be a Pair of Vals
-  LSec v b -> do locMArit a1 (biop b (val_arity v) a1 <> " " <> val v)
-  RSec b v -> do locMArit a1 (val v <> " " <> biop b (val_arity v) a1)
+  LSec v b -> do locM a1 (biop b (val_arity v) a1 <> " " <> val v)
+  RSec b v -> do locM a1 (val v <> " " <> biop b (val_arity v) a1)
   Scale rn -> do lfunM (LSec (Scalar rn) Outer) a1
   Zero -> do lfunM (Scale 0) a1
   Prj i j -> case (i,j,a1) of
-                (2, 1, P a3 _) -> do locMArit a3 "fst"
-                (2, 2, P _ a2) -> do locMArit a2 "snd"
+                (2, 1, P a3 _) -> do locM a3 "fst"
+                (2, 2, P _ a2) -> do locM a2 "snd"
                 _ -> undefined
   Lplus lf2 lf1 -> do lfunM (Para lf2 lf1) a1
-                      a4 <- getArity
-                      c2 <- getCount
+                      (c2, a4) <- getCountArity
                       case a4 of
-                        (P a3 a2) -> do locMArit a4 ("plus" <> arityext a3 a2 <> " " <> "fun" <> show(c2-1))
+                        (P a3 a2) -> do locM a4 ("plus" <> arityext a3 a2 <> " " <> "fun" <> show(c2-1))
                         _ -> undefined
   Red _ -> undefined
   Add _ -> undefined
   LMap _ -> undefined
   Zip _ -> undefined
-
-
-
 
 programM :: LFun -> Val -> Compiler Program
 programM lf v = let arit = val_arity v in
--- futhy/test/Compiler-tests.hs
+++ futhy/test/Compiler-tests.hs
@@ -10,17 +10,12 @@ import Compiler
 main :: IO ()
 main = defaultMain $ localOption (mkTimeout 1000000) tests
 
-just_lfun :: LFun -> String
-just_lfun f = let (_, r, _) = lfun f 1 (Atom 0) in r
 
 goodCaseProgram :: TestName -> LFun -> Val -> String -> TestTree
 goodCaseProgram name lf vin vout = testCase name $ program lf vin @?= vout
 
 goodCaseVal :: TestName -> Val -> String -> TestTree
 goodCaseVal name vin vout = testCase name $ val vin @?= vout
-
-goodCaseLFun :: TestName -> LFun -> String -> TestTree
-goodCaseLFun name lf vout = testCase name $ just_lfun lf @?= vout
 
 -- TODO
   -- use quickcheck? to test that compiler + executor gives same result as interpretor
@@ -37,28 +32,36 @@ tests = testGroup "compiler"
               (Pair (Tensor [Tensor [Scalar 3.0, Scalar 6.0], Tensor [Scalar 9.0, Scalar 12.0]]) (Tensor [Tensor [Scalar 3.0, Scalar 6.0], Tensor [Scalar 9.0, Scalar 12.0]]))
               "([[3.0f32, 6.0f32], [9.0f32, 12.0f32]],[[3.0f32, 6.0f32], [9.0f32, 12.0f32]])"
             ]
-          , testGroup "lfun tests"
-            [ goodCaseLFun "Id"
-              (Id)
-              "let fun1 = (id)\n"
-            , goodCaseLFun "Id"
-              (Dup)
-              "let fun1 = (dupe)\n"
-            , goodCaseLFun "scale"
-              (Scale 3.0)
-              "let fun1 = (outer_0_0 3.0f32)\n"
-            , goodCaseLFun "comp"
-              (Comp  (Comp  (Scale 3.0) (Scale 7.0)) (Scale 8.0))
-              "let fun3 = (outer_0_0 3.0f32)\nlet fun2 = (outer_0_0 7.0f32)\nlet fun4 = (comp fun3 fun2)\nlet fun1 = (outer_0_0 8.0f32)\nlet fun5 = (comp fun4 fun1)\n"
-            , goodCaseLFun "para"
-              (Comp  (Para  (Scale 3.0) (Scale 7.0)) Dup)
-              "let fun3 = (outer_0_0 3.0f32)\nlet fun2 = (outer_0_0 7.0f32)\nlet fun4 = (para fun3 fun2)\nlet fun1 = (dupe)\nlet fun5 = (comp fun4 fun1)\n"
-            , goodCaseLFun "lplus"
-              (Comp  (Lplus  (Scale 3.0) (Scale 7.0)) Dup)
-              "let fun3 = (outer_0_0 3.0f32)\nlet fun2 = (outer_0_0 7.0f32)\nlet fun4 = (para fun3 fun2)\nlet fun5 = (plus_0_0 fun4)\nlet fun1 = (dupe)\nlet fun6 = (comp fun5 fun1)\n"
-            ]
           , testGroup "whole program tests"
-            [ goodCaseProgram "Id scalar"
+            [ goodCaseProgram "Id"
+              (Id)
+              (Scalar 1.0)
+              "open import \"lmaplib\"\n\nlet arg = 1.0f32\n\nlet fun1 = (id)\nentry main = fun1 arg"
+            , goodCaseProgram "Dupe"
+              (Dup)
+              (Scalar 1.0)
+              "open import \"lmaplib\"\n\nlet arg = 1.0f32\n\nlet fun1 = (dupe)\nentry main = fun1 arg"
+            , goodCaseProgram "scale scalar"
+              (Scale 3.0)
+              (Scalar 1.0)
+              "open import \"lmaplib\"\n\nlet arg = 1.0f32\n\nlet fun1 = (outer_0_0 3.0f32)\nentry main = fun1 arg"
+            , goodCaseProgram "scale marix"
+              (Scale 3.0)
+              (Tensor [Tensor [Scalar 1.0, Scalar 2.0], Tensor [Scalar 3.0, Scalar 4.0]])
+              "open import \"lmaplib\"\n\nlet arg = [[1.0f32, 2.0f32], [3.0f32, 4.0f32]]\n\nlet fun1 = (outer_0_2 3.0f32)\nentry main = fun1 arg"
+            , goodCaseProgram "comp"
+              (Comp  (Comp  (Scale 3.0) (Scale 7.0)) (Scale 8.0))
+              (Scalar 1.0)
+              "open import \"lmaplib\"\n\nlet arg = 1.0f32\n\nlet fun1 = (outer_0_0 8.0f32)\nlet fun2 = (outer_0_0 7.0f32)\nlet fun3 = (outer_0_0 3.0f32)\nlet fun4 = (comp fun3 fun2)\nlet fun5 = (comp fun4 fun1)\nentry main = fun5 arg"
+            , goodCaseProgram "para"
+              (Comp  (Para  (Scale 3.0) (Scale 7.0)) Dup)
+              (Scalar 1.0)
+              "open import \"lmaplib\"\n\nlet arg = 1.0f32\n\nlet fun1 = (dupe)\nlet fun2 = (outer_0_0 7.0f32)\nlet fun3 = (outer_0_0 3.0f32)\nlet fun4 = (para fun3 fun2)\nlet fun5 = (comp fun4 fun1)\nentry main = fun5 arg"
+            , goodCaseProgram "lplus"
+              (Comp  (Lplus  (Scale 3.0) (Scale 7.0)) Dup)
+              (Scalar 1.0)
+              "open import \"lmaplib\"\n\nlet arg = 1.0f32\n\nlet fun1 = (dupe)\nlet fun2 = (outer_0_0 7.0f32)\nlet fun3 = (outer_0_0 3.0f32)\nlet fun4 = (para fun3 fun2)\nlet fun5 = (plus_0_0 fun4)\nlet fun6 = (comp fun5 fun1)\nentry main = fun6 arg"
+            , goodCaseProgram "Id scalar"
               (Id)
               (Scalar 15.0)
               "open import \"lmaplib\"\n\nlet arg = 15.0f32\n\nlet fun1 = (id)\nentry main = fun1 arg"
