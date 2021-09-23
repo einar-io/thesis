84a80027e5957c82ea1811c6103c8f31fa351142
Einar Rasmussen <einar@di.ku.dk>
2021-09-23 12:49:15 +0200

    More interpretor tests.

--- futhy/src/Interpretor.hs
+++ futhy/src/Interpretor.hs
@@ -43,9 +43,11 @@ proj2 _ = undefined
 
 vectorspacePlus :: Val -> Val -> Val
 vectorspacePlus left right = case (left, right) of
-   (Scalar l, Scalar r)     -> Scalar $ l + r
-   (Tensor ls, Tensor rs)   -> Tensor $ zipWith vectorspacePlus ls rs
-   (Pair ll lr, Pair rl rr) -> Pair (ll `vectorspacePlus` rl) (lr `vectorspacePlus` rr)
+   (Zero, r@(Scalar _))       -> r
+   (l@(Scalar _), Zero)       -> l
+   (Scalar l, Scalar r)       -> Scalar $ l + r
+   (Tensor ls, Tensor rs)     -> Tensor $ zipWith vectorspacePlus ls rs
+   (Pair ll lr, Pair rl rr)   -> Pair (ll `vectorspacePlus` rl) (lr `vectorspacePlus` rr)
    _ -> undefined
 
 {- vecLookup, flipLookup, keepValues, reduce are all helper functions to the reduce operator -}
@@ -91,7 +93,7 @@ interpret f v = case (f, v) of
   (LSec l op, _) -> Right $ applyOp op l v
   (RSec op r, _) -> Right $ applyOp op v r
   (Scale s, _)   -> Right $ outer (Scalar s) v
-  (KZero, _)     -> Right $ Scalar 0.0 -- can this be done better?
+  (KZero, _)     -> Right Zero -- Scalar 0.0 -- can this be done better?
   (Prj 2 1, _) -> Right $ proj1 v
   (Prj 2 2, _) -> Right $ proj2 v
   (Prj _ _, _) -> Left "Invalid argument to Prj"
@@ -104,20 +106,21 @@ interpret f v = case (f, v) of
 
   (Neg   , _) -> Right $ negate v
 
-  (LMap _, _) -> Left "Invalid argument to LMap"
+  (LMap fn, VList vs) -> do vals <- mapM (interpret fn) vs
+                            return (VList vals)
 
-  (Zip fs, Tensor vs) -> case length fs `compare` length vs of
-                           EQ -> do vals <- zipWithM interpret fs vs
-                                    Right $ Tensor vals
-                           _  -> Left "Invalid argument pair to Zip.  Lists of LFUNS and list of values must have same length."
+  (Zip fs, VList vs) -> if length fs == length vs
+                        then do vals <- zipWithM interpret fs vs
+                                return (VList vals)
+                        else Left "Invalid argument pair to Zip.  Lists of LFUNS and VLIST must have same length."
 
-  (Add, Pair Zero vr) -> Right vr
-  (Add, Pair vl Zero) -> Right vl
-  (Add, Pair (Scalar l) (Scalar r)) -> Right $ Scalar (l+r)
-  (Add, Pair (Tensor l) (Tensor r)) -> case length l `compare` length r of
-                                         EQ -> do vs <- zipWithM (\x y -> interpret Add (Pair x y)) l r
-                                                  Right $ Tensor vs
-                                         _  -> Left "Invalid pair of tensors. Lists values do not have same length."
+  (Add, Pair Zero vr) -> return vr
+  (Add, Pair vl Zero) -> return vl
+  (Add, Pair (Scalar l) (Scalar r)) -> return $ Scalar (l+r)
+  (Add, Pair (Tensor l) (Tensor r)) -> if length l == length r
+                                       then do vs <- zipWithM (\x y -> interpret Add (Pair x y)) l r
+                                               return (Tensor vs)
+                                       else Left "Invalid pair of tensors. Lists values do not have same length."
   (Add, _) ->  Left "Invalid argument to Add"
 
 eval :: LFun -> Val -> String
